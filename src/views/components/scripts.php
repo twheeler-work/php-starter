c9b036ab565cec11e7385eb0d48f5c8dbae95bf4
twheeler-work <thomas.wheeler@nrg.com>
2020-04-20 14:21:55 -0500

    Cleaned up common

--- src/views/components/scripts.php
+++ src/views/components/scripts.php
@@ -1,5 +1,6 @@
-<!-- Alert Box -->
-<div id="alert"></div>
+<!-- Error message-->
+<?php require_once($links['ROOT']['common'] . "error.message.php") ?>
+
 <!-- Scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
