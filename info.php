3675d081277b7b57384382b0661e79b633b8fc0b
twheeler-work <thomas.wheeler@nrg.com>
2020-05-13 07:55:01 -0500

    Adding analytics

--- info.php
+++ info.php
@@ -3,6 +3,7 @@
 return [
     // Site info
     'TITLE' => 'PHP Starter',
+    'PROD-URL' => 'www.phpstarter.com',
     'VERSION' => '1.0',
     // Pages directory
     "VIEWS" => "src/views/"
--- src/views/components/header.php
+++ src/views/components/header.php
@@ -8,5 +8,10 @@
 <!-- Bootstrap -->
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+<!-- Analytics -->
+<?php if ($_SERVER["SERVER_NAME"] === $site["PROD-URL"]) : ?>
+    <script src="//assets.adobedtm.com/bdc402f2525d/606bc8a34917/launch-b007c9afd75d.min.js" async></script>​
+<?php endif ?>
+
 <!-- Custom -->
 <link rel="stylesheet" href="<?= $links['ROOT']['css'] ?>core.css?v=<?= $site['VERSION'] ?>" />
